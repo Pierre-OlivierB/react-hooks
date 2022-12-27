daf529bb4ab8ff41e891d177ce29f2613bf4b28f
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2022-12-27 16:56:34 +0100

    try usecontext usefull for id co

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
+import { UserNumber } from "./components/AppContext";
+import OtherComponent from "./components/OtherComponent";
 
 const App = () => {
   // State
@@ -11,25 +13,28 @@ const App = () => {
   }, [myNumber]);
 
   return (
-    <div>
-      <p>{myNumber}</p>
-      {/* verify type of response */}
-      <button onClick={() => setMyNumber(Number(myNumber) + 1)}>
-        Incrémenter + 1
-      </button>
-      <label htmlFor="">Numéro de départ</label>
-      <input
-        type="number"
-        defaultValue={0}
-        ref={selectedNumber}
-        // Effect :
-        // onChange={(e) => setMyNumber(e.target.value)}
-        // Ref : (recommanded for input and form)
-        onChange={() => setMyNumber(selectedNumber.current.value)}
-        name=""
-        id=""
-      />
-    </div>
+    <UserNumber.Provider value={myNumber}>
+      <div>
+        <p>{myNumber}</p>
+        {/* verify type of response */}
+        <button onClick={() => setMyNumber(Number(myNumber) + 1)}>
+          Incrémenter + 1
+        </button>
+        <label htmlFor="">Numéro de départ</label>
+        <input
+          type="number"
+          defaultValue={0}
+          ref={selectedNumber}
+          // Effect :
+          // onChange={(e) => setMyNumber(e.target.value)}
+          // Ref : (recommanded for input and form)
+          onChange={() => setMyNumber(selectedNumber.current.value)}
+          name=""
+          id=""
+        />
+      </div>
+      <OtherComponent />
+    </UserNumber.Provider>
   );
 };
 
---
+++ src/components/AppContext.js
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+// box stock in hight lvl
+
+export const UserNumber = createContext(0);
